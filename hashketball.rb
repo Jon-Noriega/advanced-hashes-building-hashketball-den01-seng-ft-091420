3af0108c844636649d6a36d884c5a52d8520c7bf
Jon Noriega <noriega331@gmail.com>
2020-08-27 18:13:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,18 @@
-# Write your code here!
+require "pry"
+
+def game_hash(){
+    :home => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Purple","Turqoise"],
+      :players => "Skins"
+      },
+    :away => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black","White"],
+      :players => "Ts"
+    }
+  }
+end
+
+binding.pry
+
