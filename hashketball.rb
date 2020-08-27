dd07286d274dd3a68322178593ca1a9b81738760
Jon Noriega <noriega331@gmail.com>
2020-08-27 18:29:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,6 +17,17 @@ def game_hash(){
           :steals => 7,
           :blocks => 8,
           :slam_dunks => 9
+        },
+        {"Reggie Evans" => {
+          :player_name => 1,
+          :number => 2,
+          :shoe => 3,
+          :points => 4,
+          :rebounds => 5,
+          :assists => 6,
+          :steals => 7,
+          :blocks => 8,
+          :slam_dunks => 9
         }
         ]
     }
