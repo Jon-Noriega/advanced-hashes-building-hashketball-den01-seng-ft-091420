0e3e9e7d60a9420ac8ee2e619a269ce3395e17fd
Jon Noriega <noriega331@gmail.com>
2020-08-27 19:23:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -106,10 +106,10 @@ def game_hash(){
           :shoe => 15,
           :points => 33,
           :rebounds => 3,
-          :assists => 6,
-          :steals => 3,
-          :blocks => 8,
-          :slam_dunks => 5
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0
           },
           
           {:player_name => "Kemba Walker",
