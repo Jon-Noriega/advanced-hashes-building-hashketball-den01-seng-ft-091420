3e1a5339db3f8297211f9dd3e932a1d5f7dc9fdb
Jon Noriega <noriega331@gmail.com>
2020-08-27 18:59:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,7 +11,7 @@ def game_hash(){
           :shoe => 16,
           :points => 22,
           :rebounds => 12,
-          :assists => 6,
+          :assists => 12,
           :steals => 7,
           :blocks => 8,
           :slam_dunks => 9
